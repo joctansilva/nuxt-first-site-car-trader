3b6b7e1f2d13f6bf2a648679f39f9d16171f282d
Joctan Neves <joctan.silva@trinusco.com.br>
2023-11-04 11:25:49 -0300

    feat: adding useHead fuction in the pages

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,3 +1,17 @@
+<script setup>
+const route = useRoute();
+
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+useHead({
+  title: toTitleCase(route.params.name),script
+});
+
+</script>
+
 <template>
   <div>
     <CarDetailPage />
