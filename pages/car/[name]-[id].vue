b89ffc0685fb7f12dfe4534bc5dc7adc33f84233
Joctan Neves <joctan.silva@trinusco.com.br>
2023-11-04 10:43:11 -0300

    feat: create a car nuxt page

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        <CarDetailPage />
-    </div>
+  <div>
+    <CarDetailPage />
+  </div>
 </template>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <CarPage />
+    <CarCards />
   </div>
 </template>
