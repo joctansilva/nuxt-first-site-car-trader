b68cf9457f7918682c72db04f3f900e396c6c5cb
Joctan Neves <joctan.silva@trinusco.com.br>
2023-11-04 22:04:40 -0300

    feat: include json, and consume them

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,23 +1,24 @@
+<script setup>
+  const props = defineProps({
+    car: Object,
+  });
+</script>
+
 <template>
   <div
     class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    @click="navigateTo(`/car/${car.name}-${car.id}`)"
   >
     <div class="flex h-full">
-      <img
-        src="https://quatrorodas.abril.com.br/wp-content/uploads/2020/04/volvo_xc40_t5_plug-in_hybrid_r-design_41-e1606940243985.jpg?quality=70&strip=info"
-        alt="volvo_xc40_t5_plug-in_hybrid_r-design_41-e1606940243985"
-        class="w-[300px] h-full"
-      />
+      <img :src="car.url" :alt="car.name" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">Volvo XC40</h1>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
           <p class="text-gray-700">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quas
-            voluptas sint enim totam sapiente ipsa id fugit beatae. Voluptatem,
-            at quod molestias ad labore mollitia ratione quo minus iure?
+            {{ car.description }}
           </p>
         </div>
-        <h1 class="mt-auto text-xl">$39,555</h1>
+        <h1 class="mt-auto text-xl">{{ car.price }}</h1>
       </div>
     </div>
   </div>
